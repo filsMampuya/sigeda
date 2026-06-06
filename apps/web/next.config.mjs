405a2cd4d41adf0d4e376077ad9be1b56eafbec9
filsMampuya <fils.mampuya.2@gmail.com>
2026-06-06 14:00:46 +0100

    Add environment variables configuration to next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,20 @@
+const envKeys = [
+  "SIGEDA_API_URL",
+  "NEXT_PUBLIC_SIGEDA_API_URL",
+  "NEXT_PUBLIC_FIREBASE_API_KEY",
+  "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
+  "NEXT_PUBLIC_FIREBASE_PROJECT_ID",
+  "NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
+  "NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
+  "NEXT_PUBLIC_FIREBASE_APP_ID",
+  "NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID"
+];
+
+const env = Object.fromEntries(envKeys.map((key) => [key, process.env[key]]));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env
+};
 
 export default nextConfig;
